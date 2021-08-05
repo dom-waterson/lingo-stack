3f7c8e1c35b7173fed4b59081976672915b3ad5a
dom-waterson <dom_waterson@hotmail.com>
2021-08-05 11:37:15 +0100

    updated create modal to use react hook form

--- components/CreateModal.tsx
+++ components/CreateModal.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-
 import {
   Button,
   Modal,
@@ -9,39 +8,126 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Textarea,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
 } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+
+import { uniqueWords } from 'foreign-text-parser'
 
 type CreateModalProps = {
-  children: React.ReactNode
-  handleCreate: () => void
   title: string
   handleClose: () => void
   isOpen: boolean
 }
 
+type FormValues = {
+  name: string
+  text: string
+  numberOfWords: number
+}
+
 const CreateModal: React.FC<CreateModalProps> = ({
-  children,
-  handleCreate,
   title,
   handleClose,
   isOpen,
-}) => (
-  <Modal size="xl" isOpen={isOpen} onClose={handleClose}>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>{title}</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>{children}</ModalBody>
-      <ModalFooter display="flex" justifyContent="space-between">
-        <Button colorScheme="blue" mr={3} onClick={handleClose}>
-          Close
-        </Button>
-        <Button onClick={handleCreate} variant="ghost">
-          Create
-        </Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
-)
+}) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  const handleCreate = async ({ text, numberOfWords, name }: FormValues) => {
+    const result = await uniqueWords(text, numberOfWords)
+
+    const payload = { name, words: result }
+
+    console.log(payload)
+    handleClose()
+  }
+
+  return (
+    <Modal size="xl" isOpen={isOpen} onClose={handleClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <form onSubmit={handleSubmit(handleCreate)}>
+          <ModalHeader>{title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl paddingBottom={4} isInvalid={errors.name}>
+              <FormLabel htmlFor="name">Stack name</FormLabel>
+              <Input
+                id="name"
+                placeholder="Harry Potter: chapter one paragraph words"
+                {...register('name', {
+                  required: 'This stack needs a name',
+                })}
+              />
+              <FormErrorMessage>
+                {errors.name && errors.name.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <FormControl paddingBottom={4} isInvalid={errors.text}>
+              <FormLabel htmlFor="text">
+                Enter text to parse words from
+              </FormLabel>
+              <Textarea
+                id="text"
+                {...register('text', {
+                  required: 'Need to include text to get words from',
+                })}
+                resize="none"
+                placeholder="El señor y la señora Dursley ..."
+                size="lg"
+              />
+              <FormErrorMessage>
+                {errors.text && errors.text.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <FormControl paddingBottom={4} isInvalid={errors.numberOfWords}>
+              <FormLabel htmlFor="numberOfWords">
+                Enter number of words to find
+              </FormLabel>
+              <NumberInput min={1}>
+                <NumberInputField
+                  placeholder="10"
+                  {...register('numberOfWords', {
+                    required: 'Need to know the number of words to find',
+                  })}
+                />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <FormErrorMessage>
+                {errors.numberOfWords && errors.numberOfWords.message}
+              </FormErrorMessage>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter display="flex" justifyContent="space-between">
+            <Button colorScheme="blue" mr={3} onClick={handleClose}>
+              Close
+            </Button>
+            <Button isLoading={isSubmitting} type="submit" variant="ghost">
+              Create
+            </Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </Modal>
+  )
+}
 
 export default CreateModal
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,48 +1,16 @@
 import React from 'react'
-import {
-  Box,
-  Button,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  Textarea,
-  useDisclosure,
-} from '@chakra-ui/react'
-import { uniqueWords } from 'foreign-text-parser'
+import { Box, Button, useDisclosure } from '@chakra-ui/react'
 
 import CreateModal from '@/components/CreateModal'
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
-  let [value, setValue] = React.useState<string>()
-  let [results, setResults] = React.useState<string[]>()
-  let [num, setNum] = React.useState<number>(15)
-
-  let handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    let inputValue = e.target.value
-    setValue(inputValue)
-  }
-
-  let handleNumChange = (_: string, result: number) => {
-    setNum(result)
-  }
-
-  let handleCreate = async () => {
-    if (!value) return
-
-    const result = await uniqueWords(value, num)
-
-    setResults(result)
-    onClose()
-  }
   return (
     <Box
       d="flex"
       justifyContent="flex-end"
-      bg="tomato"
+      bg="gray.700"
       w="100%"
       p={4}
       color="white"
@@ -51,27 +19,7 @@ const Header = () => {
         Create stack
       </Button>
 
-      <CreateModal
-        isOpen={isOpen}
-        handleCreate={handleCreate}
-        handleClose={onClose}
-        title="Create stack"
-      >
-        <Textarea
-          value={value}
-          onChange={handleInputChange}
-          resize="none"
-          placeholder="Text to generate word stack"
-          size="lg"
-        />
-        <NumberInput value={num} onChange={handleNumChange}>
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-      </CreateModal>
+      <CreateModal isOpen={isOpen} handleClose={onClose} title="Create stack" />
     </Box>
   )
 }
